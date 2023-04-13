d96d89254785316322c3f45cd358bbc801b73679
hamdtc <mohamedtc73@gmail.com>
2023-04-13 17:31:26 +0530

    test

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import Card from "./componets/Card";
 import cardData from "./data/Data";
 
 function App() {
+  const cardElements = cardData.map((data) => {
+    return <Card key={data.id} item={data} />;
+  });
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <section className="cards-list">{cardElements}</section>
 
       <div> this is app</div>
     </div>
